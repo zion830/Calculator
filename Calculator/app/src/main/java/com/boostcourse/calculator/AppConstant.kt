434dce923e2480cbef202cc72f2fa66d3886c34f
zion830 <zion830@honglab.org>
2019-09-26 17:06:00 +0900

    연산자 constant 추가

---
+++ Calculator/app/src/main/java/com/boostcourse/calculator/AppConstant.kt
@@ -0,0 +1,10 @@
+package com.boostcourse.calculator
+
+const val NONE = "none"
+const val ADD = "add"
+const val SUB = "sub"
+const val MULTI = "multi"
+const val DIVIDE = "divide"
+const val POWER = "power"
+const val PLUS_MINUS = "plus_minus"
+const val PERCENT = "percent"
