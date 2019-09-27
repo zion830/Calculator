ec80988a3b1f2e0ec171ee594b9984eb0b1d8468
zion830 <zion830@honglab.org>
2019-09-27 13:06:58 +0900

    연산자 버튼 이벤트 구현

--- Calculator/app/src/main/java/com/boostcourse/calculator/util/NumberFormat.kt
+++ Calculator/app/src/main/java/com/boostcourse/calculator/util/NumberFormat.kt
@@ -1,13 +1,28 @@
 package com.boostcourse.calculator.util
 
-import java.text.DecimalFormat
-
 object NumberFormat {
+    fun stringToDouble(num: String) = if (num.indexOf(".") == num.length - 1) {
+        num.substring(0, num.length - 1).toDouble()
+    } else {
+        num.toDouble()
+    }
 
-    fun checkValue(num: String): String =
-        if (num == ".") {
-            "0."
-        } else {
-            num
+    fun doubleToString(num: Double?): String {
+        num?.let {
+            val value = num.toString()
+            return if (value.indexOf(".0") == value.length - 2) {
+                value.substring(0, value.length - 2)
+            } else {
+                value
+            }
         }
+
+        return ""
+    }
+
+    fun checkValue(num: String): String = if (num == ".") "0." else num
+
+    fun checkPoint(num: String) = num.length - num.replace(".", "").length > 1
+
+    fun checkLength(num: String) = num.replace(".", "").length > 15
 }
--- Calculator/app/src/main/java/com/boostcourse/calculator/view/MainActivity.kt
+++ Calculator/app/src/main/java/com/boostcourse/calculator/view/MainActivity.kt
@@ -1,42 +1,43 @@
 package com.boostcourse.calculator.view
 
 import android.os.Bundle
-import android.text.method.ScrollingMovementMethod
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.boostcourse.calculator.R
-import com.boostcourse.calculator.util.Calculator
+import com.boostcourse.calculator.*
+import com.boostcourse.calculator.util.CalculatorCallback
+import com.boostcourse.calculator.util.CalculatorImpl
 import com.boostcourse.calculator.util.NumberFormat
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-    private var currentValue = ""
-    private val calculator = Calculator()
+class MainActivity : AppCompatActivity(), CalculatorCallback {
+    private lateinit var calculator: CalculatorImpl
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        calculator = CalculatorImpl(this)
         initView()
     }
 
     private fun initView() {
-        tv_main_input.movementMethod = ScrollingMovementMethod()
 
-        for (btn in getNumberBtns()) {
-            btn.setOnClickListener {
-                addInputValue(btn.text.toString())
-            }
+        getNumberBtns().forEach {
+            it.setOnClickListener { _ -> addInputValue(it.text.toString()) }
         }
 
         btn_plus_minus.setOnClickListener {
+            val input = tv_main_input.text.toString()
 
-        }
-
-        btn_power.setOnClickListener {
+            if (input.isEmpty()) {
+                showMsg("값을 입력해주세요.")
+            } else {
+                val trans = NumberFormat.stringToDouble(input) * -1
+                displayValue(NumberFormat.doubleToString(trans))
+            }
         }
 
         btn_clear.setOnClickListener {
-            clearValue()
             calculator.reset()
         }
 
@@ -45,25 +46,28 @@ class MainActivity : AppCompatActivity() {
         }
 
         btn_back.setOnLongClickListener {
-            clearValue()
+            displayValue("")
             true
         }
 
         btn_add.setOnClickListener {
-            tv_main_input.text = calculator.add(currentValue)
-            currentValue = ""
+            calculator.performOperation(ADD, tv_main_input.text.toString())
         }
 
         btn_sub.setOnClickListener {
-
+            calculator.performOperation(SUB, tv_main_input.text.toString())
         }
 
         btn_multi.setOnClickListener {
-
+            calculator.performOperation(MULTI, tv_main_input.text.toString())
         }
 
         btn_divide.setOnClickListener {
+            calculator.performOperation(DIVIDE, tv_main_input.text.toString())
+        }
 
+        btn_power.setOnClickListener {
+            calculator.performOperation(POWER, tv_main_input.text.toString())
         }
 
         btn_point.setOnClickListener {
@@ -71,28 +75,42 @@ class MainActivity : AppCompatActivity() {
         }
 
         btn_result.setOnClickListener {
-            tv_main_input.text = calculator.displayResult(currentValue)
-            currentValue = ""
+            if (tv_main_input.text.isEmpty()) {
+                showMsg("값을 입력해주세요.")
+            } else {
+                calculator.setValue(tv_main_input.text.toString())
+                calculator.calculate()
+            }
         }
     }
 
     private fun addInputValue(letter: String) {
-        currentValue = NumberFormat.checkValue(currentValue + letter)
-        tv_main_input.text = currentValue
+        val current = tv_main_input.text.toString() + letter
+
+        if (NumberFormat.checkPoint(current)) {
+            showMsg("이미 . 연산자를 사용했습니다")
+        } else if (NumberFormat.checkLength(current)) {
+            showMsg("15자리까지 입력할 수 있습니다.")
+        } else {
+            displayValue(NumberFormat.checkValue(current))
+        }
     }
 
     private fun backspaceValue() {
+        val currentValue = tv_main_input.text
+
         if (currentValue.isNotBlank()) {
-            currentValue = currentValue.substring(0, currentValue.length - 1)
+            tv_main_input.text = currentValue.substring(0, currentValue.length - 1)
         }
-
-        tv_main_input.text = currentValue
-    }
-
-    private fun clearValue() {
-        tv_main_input.text = calculator.add(currentValue)
-        currentValue = ""
     }
 
     private fun getNumberBtns() = arrayOf(btn_0, btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9)
+
+    override fun displayValue(result: String) {
+        tv_main_input.text = result
+    }
+
+    override fun showMsg(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
 }
